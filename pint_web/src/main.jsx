0152f9a55d7dd6131759344560251501222dad31
vsko2014 <vasco1900c@gmail.com>
2026-04-15 09:36:32 +0100

    UPDATE Inicial

--- pint_web/src/main.jsx
+++ pint_web/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Importação do Bootstrap Base
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Se baixares o template oficial, aqui importarias o ficheiro customizado:
+import './assets/scss/hope-ui.scss' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
