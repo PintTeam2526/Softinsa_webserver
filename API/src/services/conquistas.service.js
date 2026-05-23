0a5d481da27ea7f723218cf489e03aa7c6b296de
vsko2014 <vasco1900c@gmail.com>
2026-05-23 22:11:12 +0100

    UPDATE Cons - Conquistas integradas
    -> problemas de back corrigidos
    -> falta testar: mudança de pontos / badges obtidos

--- API/src/services/conquistas.service.js
+++ API/src/services/conquistas.service.js
@@ -1,23 +1,37 @@
 const ConquistasConsultores = require('../models/ConquistasConsultores.models');
+const Conquistas = require('../models/Conquistas.models');
 const Consultores = require('../models/Consultores.models');
 const BadgesConcluidos = require('../models/BadgesConcluidos.models');
 
 const service = {};
 
 service.criarConquista = async (id_consultor, id_conquista) => {
-    ConquistasConsultores.create({
-            id_consultor : id_consultor,
-            id_conquista: id_conquista,
-            progresso: 0,
-        });
-}
+    await ConquistasConsultores.create({
+        id_consultor,
+        id_conquista,
+        progresso: 0,
+    });
+};
 
-service.findByIdConsultor = async(id_consultor) => {
-    return ConquistasConsultores.findAll({
-        where: {
-            id_consultor: id_consultor
-        }
-    })
-}
+service.findByIdConsultor = async (id_consultor) => {
+    const todasConquistas = await Conquistas.findAll();
+
+    const progressoConsultor = await ConquistasConsultores.findAll({
+        where: { id_consultor }
+    });
+
+    const progressoMap = {};
+    progressoConsultor.forEach((cc) => {
+        progressoMap[cc.id_conquista] = cc.progresso;
+    });
+
+    return todasConquistas.map((conquista) => ({
+        id_conquista_consultor: null,
+        id_consultor,
+        id_conquista: conquista.id_conquista,
+        progresso: progressoMap[conquista.id_conquista] ?? 0,
+        Conquista: conquista,
+    }));
+};
 
 module.exports = service;
