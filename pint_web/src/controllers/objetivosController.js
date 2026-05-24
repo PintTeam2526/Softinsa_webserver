26d5fcb28bf31b0fbd34ecc1c14e441d4d5704cd
vsko2014 <vasco1900c@gmail.com>
2026-05-24 17:51:43 +0100

    UPDATE Cons - view de Objetivos integrada
    -> ver se tem a logica de obter badge com obter objetivo implementada

---
+++ pint_web/src/controllers/objetivosController.js
@@ -0,0 +1,16 @@
+import api from '../services/api'
+
+export const getMeusObjetivos = async () => {
+    const response = await api.get('/objetivos/meus')
+    return response.data
+}
+
+export const getBadgesDisponiveisObjetivos = async () => {
+    const response = await api.get('/objetivos/badges-disponiveis')
+    return response.data
+}
+
+export const criarObjetivo = async (payload) => {
+    const response = await api.post('/objetivos/criar', payload)
+    return response.data
+}
