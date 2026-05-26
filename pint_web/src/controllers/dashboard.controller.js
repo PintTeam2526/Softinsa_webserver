31de5e036289ffebc1e39f7919d427b09be42b8c
vsko2014 <vasco1900c@gmail.com>
2026-05-26 22:03:39 +0100

    UPDATE Tm - view Dashboard (100% integrada)

--- pint_web/src/controllers/dashboard.controller.js
+++ pint_web/src/controllers/dashboard.controller.js
@@ -25,3 +25,15 @@ export async function getDashboardConsultor() {
     throw error
   }
 }
+
+// Para chamar a rota do back com os dados do TM
+export async function getDashboardTM() {
+  try {
+    const response = await api.get('/dashboard/tm')
+    console.log('Dashboard TM data:', response.data) // RETIRAR DEPOIS
+    return response.data
+  } catch (error) {
+    console.error('Erro ao obter dados da dashboard', error)
+    throw error
+  }
+}
