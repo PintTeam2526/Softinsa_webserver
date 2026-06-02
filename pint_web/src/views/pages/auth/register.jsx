c9b360ea97d41e63346d7a020bcb8f438887a54e
vsko2014 <vasco1900c@gmail.com>
2026-06-02 12:33:02 +0100

    Revert "UPDATE Guest V2 - login + registo"
    This reverts commit c112b50ae5ec5869dcc426f8574bfe5a3e24b9a6.

--- pint_web/src/views/pages/auth/register.jsx
+++ pint_web/src/views/pages/auth/register.jsx
@@ -79,17 +79,6 @@ function RegisterView() {
             </text>
           </svg>
         </div>
-
-        {/* Back Arrow */}
-        <button 
-          className="register-back-button" 
-          onClick={() => navigate(-1)}
-          aria-label="Voltar atrás"
-        >
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M19 12H5M12 19l-7-7 7-7" />
-          </svg>
-        </button>
       </div>
 
       <div className="register-form-side">
