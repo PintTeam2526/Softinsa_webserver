543b4c790028c7bed3c1d96a32081c80ae72f667
Rafael Carvalho <126340505+AccidentalTop@users.noreply.github.com>
2026-06-29 23:44:19 +0100

    fix do database.js

--- API/database.js
+++ API/database.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: 2
+      rejectUnauthorized: true
     }
   }
 });
